4543ae7122148a2bd52fdae5ed211bd0f9435541
projectswebworldhub009 <projectswebworldhub009@gmail.com>
2025-09-23 11:11:53 +0530

    header change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/header";
+// import Header from "./components/header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 
 function Layout() {
