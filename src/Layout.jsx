f7fa7cd2bbae09df41719adb129c2cf84549abdb
projectswebworldhub009 <projectswebworldhub009@gmail.com>
2025-09-23 11:16:29 +0530

    layout change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
-// import Header from "./components/header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-
 function Layout() {
   return (
     <>
-      
-      <Header/>
+      <Header />
       <Navbar />
       <Outlet />
       <Footer />
